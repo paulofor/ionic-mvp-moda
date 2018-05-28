94d3e4a410ebcdcdbce6f76aeb736d88dd3d13d6
Paulo Alexandre <paulofore@gmail.com>
2018-05-28 03:54:44 -0300

    Ajustando tela de produtos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { DetalheProdutoPage } from '../detalhe-produto/detalhe-produto';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +12,12 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+  produtoSelecionado(event, produto) {
+    this.navCtrl.push(DetalheProdutoPage, {
+        item: produto, 
+    })
+  }
+
 
 }
