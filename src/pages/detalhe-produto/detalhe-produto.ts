b9a79b6fb3a34aa338f6d5f0821e8761abfb5a28
Paulo Alexandre <paulofore@gmail.com>
2018-06-09 09:44:34 -0300

    Com os itens de carrrinho

--- src/pages/detalhe-produto/detalhe-produto.ts
+++ src/pages/detalhe-produto/detalhe-produto.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 
 import { Produto } from '../../app/shared/sdk/models/Produto';
 import { ProdutoApi } from '../../app/shared/sdk/services/custom/Produto';
+import { Pedido } from '../../app/shared/sdk/models/Pedido';
+import { PedidoApi } from '../../app/shared/sdk/services/custom/Pedido';
+import { UsuarioAppApi } from '../../app/shared/sdk/services/custom/UsuarioApp';
+import { ItemPedido } from '../../app/shared/sdk';
 
 /**
  * Generated class for the DetalheProdutoPage page.
@@ -18,11 +22,18 @@ import { ProdutoApi } from '../../app/shared/sdk/services/custom/Produto';
 })
 export class DetalheProdutoPage {
 
-  produto : Produto;
+  produto: Produto;
+  pedido: Pedido;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-            private toastCtrl: ToastController) {
-    this.produto = navParams.get('item');
+    private toastCtrl: ToastController, private usuarioSrv: UsuarioAppApi,
+    private pedidoSrv: PedidoApi) {
+    
+  }
+
+  ngOnInit() {
+    this.produto = this.navParams.get('item');
+    this.recuperaPedido();
   }
 
   ionViewDidLoad() {
@@ -30,15 +41,28 @@ export class DetalheProdutoPage {
   }
 
   adicionaProduto() {
-    this.toastCtrl.create({
-      message: 'Produto adicionando ao carrinho',
-      position: 'bottom',
-      duration: 3000
-    }).present();
+    let itemPedido: ItemPedido = new ItemPedido();
+    itemPedido.pedidoId = this.pedido.id;
+    itemPedido.produtoId = this.produto.id;
+    itemPedido.data_inclusao = new Date();
+    this.pedidoSrv.createItemPedidos(this.pedido.id, itemPedido)
+      .subscribe((result) => {
+        if (result) {
+          this.toastCtrl.create({
+            message: 'Produto adicionando ao carrinho',
+            position: 'bottom',
+            duration: 2500
+          }).present();
+        }
+      })
   }
 
   recuperaPedido() {
-    
+    this.usuarioSrv.getPedidos(1)
+      .subscribe((valor: Pedido[]) => {
+        this.pedido = valor[0];
+        console.log('Pedido:' + JSON.stringify(this.pedido));
+      })
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -20,9 +20,12 @@ export class HomePage {
 
   }
 
+  ionViewWillEnter() {
+    this.carregaQtdePedido();
+  }
+
   ngOnInit() {
     this.carregaListaProduto();
-    this.carregaQtdePedido();
   }
 
   carregaListaProduto() {
